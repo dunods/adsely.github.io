0c7b1ec64bd6b3b7d81e66ca90db8b80edf0ef0d
Ekrut <dono@ekrut.com>
2019-08-13 18:33:01 +0700

    update

---
+++ src/components/Navbars/DemoNavbar.jsx
@@ -0,0 +1,95 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// JavaScript plugin that hides or shows a component based on your scroll
+import Headroom from "headroom.js";
+// reactstrap components
+import {
+  Button,
+  UncontrolledCollapse,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class DemoNavbar extends React.Component {
+  componentDidMount() {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    // initialise
+    headroom.init();
+  }
+  render() {
+    return (
+      <>
+        <header className="header-global">
+          <Navbar
+            className="navbar-main navbar-transparent navbar-light headroom"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+                <img
+                  alt="..."
+                  src={require("assets/img/brand/adsely-white.png")}
+                />
+              </NavbarBrand>
+              <button className="navbar-toggler" id="navbar_global">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse navbar toggler="#navbar_global">
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <Link to="/">
+                        <img
+                          alt="..."
+                          src={require("assets/img/brand/argon-react.png")}
+                        />
+                      </Link>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </header>
+      </>
+    );
+  }
+}
+
+export default DemoNavbar;
---
+++ src/views/examples/Landing.jsx
@@ -0,0 +1,454 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classnames from "classnames";
+
+// reactstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.jsx";
+import CardsFooter from "components/Footers/CardsFooter.jsx";
+
+// index page sections
+import Download from "../IndexSections/Download.jsx";
+
+class Landing extends React.Component {
+  state = {};
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+          <div className="position-relative">
+            {/* shape Hero */}
+            <section className="section section-lg section-shaped pb-250">
+              <div className="shape shape-style-1 shape-default">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+              </div>
+              <Container className="py-lg-md d-flex">
+                <div className="col px-0">
+                  <Row>
+                    <Col lg="6">
+                      <h1 className="display-3 text-white">
+                        Increase Your Brand Awareness{" "}
+                        <span>By Using Adsely Influncer Marketing</span>
+                      </h1>
+                      <p className="lead text-white">
+                        With The combined approach of both technology and 
+                        digital strategist. We formulate the most eficient way
+                        for brand to boost their presence using influencer.
+                      </p>
+                    </Col>
+                  </Row>
+                </div>
+              </Container>
+              {/* SVG separator */}
+              <div className="separator separator-bottom separator-skew">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="none"
+                  version="1.1"
+                  viewBox="0 0 2560 100"
+                  x="0"
+                  y="0"
+                >
+                  <polygon
+                    className="fill-white"
+                    points="2560 0 2560 100 0 100"
+                  />
+                </svg>
+              </div>
+            </section>
+            {/* 1st Hero Variation */}
+          </div>
+          <section className="section section-lg pt-lg-0 mt--200">
+            <Container>
+              <Row className="justify-content-center">
+                <Col lg="12">
+                  <Row className="row-grid">
+                    <Col lg="4">
+                      <Card className="card-lift--hover shadow border-0">
+                        <CardBody className="py-5">
+                          <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                            <i className="ni ni-check-bold" />
+                          </div>
+                          <h6 className="text-primary text-uppercase">
+                            Identify
+                          </h6>
+                          <p className="description mt-3">
+                            We carefully listen and identify the needs of the 
+                            client. Propose with initial solution that may 
+                            fit to their needs.
+                          </p>
+                          <div>
+                            <Badge color="primary" pill className="mr-1">
+                              design
+                            </Badge>
+                            {/* <Badge color="primary" pill className="mr-1">
+                              system
+                            </Badge> */}
+                            <Badge color="primary" pill className="mr-1">
+                              creative
+                            </Badge>
+                          </div>
+                          {/* <Button
+                            className="mt-4"
+                            color="primary"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </Button> */}
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col lg="4">
+                      <Card className="card-lift--hover shadow border-0">
+                        <CardBody className="py-5">
+                          <div className="icon icon-shape icon-shape-success rounded-circle mb-4">
+                            <i className="ni ni-istanbul" />
+                          </div>
+                          <h6 className="text-success text-uppercase">
+                            Build
+                          </h6>
+                          <p className="description mt-3">
+                            We propose the list of influencer list 
+                            to be selected. Using technology and
+                            algorithm, we carefully curated the process.
+                          </p>
+                          <div>
+                            <Badge color="success" pill className="mr-1">
+                              business
+                            </Badge>
+                            <Badge color="success" pill className="mr-1">
+                              vision
+                            </Badge>
+                            {/* <Badge color="success" pill className="mr-1">
+                              success
+                            </Badge> */}
+                          </div>
+                          {/* <Button
+                            className="mt-4"
+                            color="success"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </Button> */}
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col lg="4">
+                      <Card className="card-lift--hover shadow border-0">
+                        <CardBody className="py-5">
+                          <div className="icon icon-shape icon-shape-warning rounded-circle mb-4">
+                            <i className="ni ni-planet" />
+                          </div>
+                          <h6 className="text-warning text-uppercase">
+                            Launch
+                          </h6>
+                          <p className="description mt-3">
+                            Once the client selected the influencer,
+                            we launch the campaign. While monitoring the process,
+                            to give best possible result.
+                          </p>
+                          <div>
+                            <Badge color="warning" pill className="mr-1">
+                              data
+                            </Badge>
+                            <Badge color="warning" pill className="mr-1">
+                               measure
+                            </Badge>
+                            {/* <Badge color="warning" pill className="mr-1">
+                              launch
+                            </Badge> */}
+                          </div>
+                          {/* <Button
+                            className="mt-4"
+                            color="warning"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </Button> */}
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+
+{/* 
+          <section className="section section-lg">
+            <Container>
+              <Row className="justify-content-center text-center mb-lg">
+                <Col lg="8">
+                  <h2 className="display-3">The amazing Team</h2>
+                  <p className="lead text-muted">
+                    According to the National Oceanic and Atmospheric
+                    Administration, Ted, Scambos, NSIDClead scentist, puts the
+                    potentially record maximum.
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="mb-5 mb-lg-0" lg="3" md="6">
+                  <div className="px-4">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                      src={require("assets/img/theme/team-1-800x800.jpg")}
+                      style={{ width: "200px" }}
+                    />
+                    <div className="pt-4 text-center">
+                      <h5 className="title">
+                        <span className="d-block mb-1">Ryan Tompson</span>
+                        <small className="h6 text-muted">Web Developer</small>
+                      </h5>
+                      <div className="mt-3">
+                        <Button
+                          className="btn-icon-only rounded-circle"
+                          color="warning"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-twitter" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="warning"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-facebook" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="warning"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-dribbble" />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col className="mb-5 mb-lg-0" lg="3" md="6">
+                  <div className="px-4">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                      src={require("assets/img/theme/team-2-800x800.jpg")}
+                      style={{ width: "200px" }}
+                    />
+                    <div className="pt-4 text-center">
+                      <h5 className="title">
+                        <span className="d-block mb-1">Romina Hadid</span>
+                        <small className="h6 text-muted">
+                          Marketing Strategist
+                        </small>
+                      </h5>
+                      <div className="mt-3">
+                        <Button
+                          className="btn-icon-only rounded-circle"
+                          color="primary"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-twitter" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="primary"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-facebook" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="primary"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-dribbble" />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col className="mb-5 mb-lg-0" lg="3" md="6">
+                  <div className="px-4">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                      src={require("assets/img/theme/team-3-800x800.jpg")}
+                      style={{ width: "200px" }}
+                    />
+                    <div className="pt-4 text-center">
+                      <h5 className="title">
+                        <span className="d-block mb-1">Alexander Smith</span>
+                        <small className="h6 text-muted">UI/UX Designer</small>
+                      </h5>
+                      <div className="mt-3">
+                        <Button
+                          className="btn-icon-only rounded-circle"
+                          color="info"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-twitter" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="info"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-facebook" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="info"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-dribbble" />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col className="mb-5 mb-lg-0" lg="3" md="6">
+                  <div className="px-4">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                      src={require("assets/img/theme/team-4-800x800.jpg")}
+                      style={{ width: "200px" }}
+                    />
+                    <div className="pt-4 text-center">
+                      <h5 className="title">
+                        <span className="d-block mb-1">John Doe</span>
+                        <small className="h6 text-muted">Founder and CEO</small>
+                      </h5>
+                      <div className="mt-3">
+                        <Button
+                          className="btn-icon-only rounded-circle"
+                          color="success"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-twitter" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="success"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-facebook" />
+                        </Button>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="success"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fa fa-dribbble" />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section> */}
+
+
+          <section className="section section-lg pt-0">
+            <Container>
+              <Card className="bg-gradient-warning shadow-lg border-0">
+                <div className="p-5">
+                  <Row className="align-items-center">
+                    <Col lg="8">
+                      <h3 className="text-white">
+                        We run the best campaign out there.
+                      </h3>
+                      <p className="lead text-white mt-3">
+                        We have successfully run project with high calibre brands
+                      </p>
+                    </Col>
+                    <Col className="ml-lg-auto" lg="3">
+                      <Button
+                        block
+                        className="btn-white"
+                        color="default"
+                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                        size="lg"
+                      >
+                        Contact us
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+              </Card>
+            </Container>
+          </section>
+
+
+        </main>
+        <CardsFooter />
+      </>
+    );
+  }
+}
+
+export default Landing;
